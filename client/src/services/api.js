ed1edb44633e11ee7201b735cef4cb48786e282f
Developer <test@example.com>
2026-05-29 12:34:36 +0530

    Update client API service

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://meeting-cost-calculator-dtnj.onrender.com/api/meetings';
+const API_URL = 'http://localhost:5000/api/meetings';
 
 export const getMeetings = async () => {
   const response = await axios.get(API_URL);
